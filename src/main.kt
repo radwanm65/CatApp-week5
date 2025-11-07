09266f26cfd7f77e09e669dbc9161562e1a5d96e
Marwan Radwan <radwanm@gmail.com>
2025-11-07 23:20:14 +0000

    add new cat

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,10 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
+    val clyde2 = Cat("Clyde", 5, 5)
     binnie.walk()
     clyde.walk()
+    clyde2.walk()
     println(binnie)
     println(clyde)
     //add a new comment
